4b7292ddb59588e67103ebc653efdda482072bdf
lj <1175259316@qq.com>
2016-08-18 08:59:13 +0800

    gg

--- PictureProcessing/PictureProcessing/TestVC/GPUImageTestVC.mm
+++ PictureProcessing/PictureProcessing/TestVC/GPUImageTestVC.mm
@@ -47,21 +47,12 @@
 
 - (void)sliderValueChange:(UISlider *)slider
 {
-<<<<<<< HEAD
 //    self.tjGPU.zoomBlurFilter.blurCenter = CGPointMake(slider.value, slider.value);
-    self.tjGPU.gaussianBlurFilter.blurRadiusInPixels = slider.value;
-    
-    __block UIImage *image;
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        image = [self.tjGPU.gaussianBlurFilter imageByFilteringImage:self.originImage];
-=======
 
     [self.sepiaFilter setValue:@(slider.value) forKey:@"blurRadiusInPixels"];
-    
     __block UIImage *image;
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         image = [self.sepiaFilter imageByFilteringImage:self.originImage];
->>>>>>> fe6072bd56bc35876a8e13a144e3a25068d6576a
         tj_dispatch_main_sync_safe(^{
             self.srcImageView.image = image;
         });
@@ -69,26 +60,21 @@
 }
 
 
+
 - (void)gpuImageTest {
     
     self.originImage = [UIImage imageNamed:self.imgName];
-<<<<<<< HEAD
+
     self.srcImageView.image = [self.tjGPU.gaussianBlurFilter imageByFilteringImage:self.originImage];
     
     self.slider1.maximumValue = 5;
     self.slider1.minimumValue = 0;
     self.slider1.value = 2;
-=======
 
     self.sepiaFilter = [[NSClassFromString(@"GPUImageGaussianBlurFilter") alloc] init];
     
     self.srcImageView.image = [self.sepiaFilter imageByFilteringImage:self.originImage];
-    
-    self.slider.maximumValue = 10;
-    self.slider.minimumValue = 0;
-    
-    self.slider.value = 2;
->>>>>>> fe6072bd56bc35876a8e13a144e3a25068d6576a
+
 }
 
 
