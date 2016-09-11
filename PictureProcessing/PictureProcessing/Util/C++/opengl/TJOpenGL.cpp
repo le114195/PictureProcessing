eebf06e93fc4fc0e49a7ae5f6d736e9062308d40
lejun <1175259316@qq.com>
2016-09-11 09:09:36 +0800

    gg

---
+++ PictureProcessing/PictureProcessing/Util/C++/opengl/TJOpenGL.cpp
@@ -0,0 +1,26 @@
+//
+//  TJOpenGL.cpp
+//  PictureProcessing
+//
+//  Created by 勒俊 on 16/9/11.
+//  Copyright © 2016年 勒俊. All rights reserved.
+//
+
+#include "TJOpenGL.hpp"
+
+
+
+void TJOpenGL::esTest()
+{
+    
+    
+}
+
+
+
+
+
+
+
+
+
---
+++ PictureProcessing/PictureProcessing/Util/C++/opengl/TJOpenGL.hpp
@@ -0,0 +1,42 @@
+//
+//  TJOpenGL.hpp
+//  PictureProcessing
+//
+//  Created by 勒俊 on 16/9/11.
+//  Copyright © 2016年 勒俊. All rights reserved.
+//
+
+#ifndef TJOpenGL_hpp
+#define TJOpenGL_hpp
+
+#include <stdio.h>
+#include <OpenGLES/ES3/gl.h>
+#include <OpenGLES/ES3/glext.h>
+#include <OpenGLES/gltypes.h>
+
+
+
+
+class TJOpenGL{
+    
+    
+public:
+    
+    void esTest();
+    
+    
+    
+private:
+    
+    
+    
+    
+
+};
+
+
+
+
+
+
+#endif /* TJOpenGL_hpp */
