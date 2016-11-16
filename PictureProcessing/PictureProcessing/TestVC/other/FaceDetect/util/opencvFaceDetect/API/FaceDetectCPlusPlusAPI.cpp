a4d57a6402247f46134f8a99388b1fd92ddee243
lj <1175259316@qq.com>
2016-11-16 12:34:12 +0800

    gg

--- PictureProcessing/PictureProcessing/TestVC/other/FaceDetect/util/opencvFaceDetect/API/FaceDetectCPlusPlusAPI.cpp
+++ PictureProcessing/PictureProcessing/TestVC/other/FaceDetect/util/opencvFaceDetect/API/FaceDetectCPlusPlusAPI.cpp
@@ -17,14 +17,17 @@ int FaceDetectCPlusPlusAPI::detectFeature(CvRect featureROI, cv::Rect* feature_b
     cvSetImageROI(imageIpl, featureROI);
     CvSeq* feature;
     
-    CvHaarClassifierCascade* cascade = (CvHaarClassifierCascade*)cvLoad(filePath.c_str(), NULL, NULL, NULL);
-    feature = cvHaarDetectObjects(imageIpl,
-                                  cascade,
-                                  storage,
-                                  face_feature.haarScaleFactor,
-                                  face_feature.haarMinNeighbours,
-                                  face_feature.haarFlags,
-                                  face_feature.minFeatureSize);
-    cvReleaseImage(&imageIpl);
+//    CvHaarClassifierCascade* cascade = (CvHaarClassifierCascade*)cvLoad(filePath.c_str(), NULL, NULL, NULL);
+//    feature = cvHaarDetectObjects(imageIpl,
+//                                  cascade,
+//                                  storage,
+//                                  face_feature.haarScaleFactor,
+//                                  face_feature.haarMinNeighbours,
+//                                  face_feature.haarFlags,
+//                                  face_feature.minFeatureSize);
+//    cvReleaseImage(&imageIpl);
 
+    
+    
+    return 0;
 }
