ba279910e666ae518832cdd95814190a3a9735b0
lj <1175259316@qq.com>
2016-11-22 11:50:55 +0800

    表情

---
+++ PictureProcessing/PictureProcessing/TestVC/other/TJ_NSCache/Controller/TJ_CacheController.m
@@ -0,0 +1,36 @@
+//
+//  TJ_CacheController.m
+//  PictureProcessing
+//
+//  Created by 勒俊 on 2016/11/22.
+//  Copyright © 2016年 勒俊. All rights reserved.
+//
+
+#import "TJ_CacheController.h"
+
+@interface TJ_CacheController ()
+
+
+
+@property (nonatomic, strong)
+
+
+@end
+
+@implementation TJ_CacheController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+
+@end
